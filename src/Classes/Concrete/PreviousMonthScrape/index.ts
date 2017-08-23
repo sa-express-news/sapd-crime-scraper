2f333f0d175bef761c17d264c12a8995756d03b2
Kia Farhang <kfarhang0@gmail.com>
2017-08-23 17:08:52 -0500

    Wrote (and wrote tests for) PreviousMonthScrape class to handle monthly scrape jobs

---
+++ src/Classes/Concrete/PreviousMonthScrape/index.ts
@@ -0,0 +1,28 @@
+import { ScrapeMachineParams } from '../../../Interfaces/ScrapeMachineParams';
+import { SAPDScrapeMachine } from '../SAPDScrapeMachine';
+
+export class PreviousMonthScrape{
+	machine: SAPDScrapeMachine;
+
+	constructor(currentDate: Date){
+		this.machine = new SAPDScrapeMachine({
+			startDate: this.calculateStartDate(currentDate),
+			endDate: this.calculateEndDate(currentDate),
+			districts: [0,1,2,3,4,5,6,7,8,9,10]
+		});
+	}
+
+	public getMachine(): SAPDScrapeMachine{
+		return this.machine;
+	}
+
+	private calculateStartDate(date: Date): Date{
+		let dateToReturn = date;
+		dateToReturn.setMonth(date.getMonth() - 1, 1);
+		return dateToReturn;
+	}
+
+	private calculateEndDate(date: Date): Date{
+		return new Date(date.getFullYear(), date.getMonth() + 1, 0);
+	}
+}
