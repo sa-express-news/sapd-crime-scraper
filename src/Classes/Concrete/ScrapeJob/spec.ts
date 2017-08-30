4b6399fd6b2a18e84fbd8e2811d275fca9679843
Kia Farhang <kfarhang0@gmail.com>
2017-08-30 11:27:12 -0500

    Moved ScrapeJob tests that require actual scraping to a separate block that requires a node env flag

--- src/Classes/Concrete/ScrapeJob/spec.ts
+++ src/Classes/Concrete/ScrapeJob/spec.ts
@@ -5,46 +5,52 @@ import * as chai from 'chai';
 import 'mocha';
 import * as sinon from 'sinon';
 const assert = chai.assert;
-sinon.assert.expose(assert, {prefix: ''});
 
 
 describe('ScrapeJob', function(){
 	this.timeout(60000);
-	let job = new ScrapeJob('06/07/2017');
+	let job = new ScrapeJob('06/07/2017', 0);
 	describe('Methods', ()=>{
 		beforeEach(()=>{
-			job = new ScrapeJob('06/07/2017');
-		});
-		describe.skip('run', function(){
-			it('fills the bag of calls for a search with calls', async ()=>{
-				const run = await job.run();
-
-				const bag: Bag<Call> = job.getCalls();
-
-				assert.isAbove(bag.size(), 0);
-			});
-
-			it('leaves the bag of calls empty on a search with no calls', async()=>{
-				job = new ScrapeJob('07/04/1776');
-				const run = await job.run();
-
-				const bag: Bag<Call> = job.getCalls();
-
-				assert.strictEqual(bag.size(), 0);
-			});
-
+			job = new ScrapeJob('06/07/2017', 0);
 		});
 
-		describe.skip('databaseAllCalls', function(){
-			it('adds every call in the bag to the db', async ()=>{
+		if(process.env.NETWORK_TEST === 'true'){
+			describe('run', function(){
+				it('fills the bag of calls for a search with calls', async ()=>{
+					const run = await job.run();
 
-				const run = await job.run();
+					const bag: Bag<Call> = job.getCalls();
 
-				const db = await job.DatabaseAllCalls();
+					assert.isAbove(bag.size(), 0);
+				});
 
-				assert.isTrue(db);
-			});
-		});
+				it('leaves the bag of calls empty on a search with no calls', async()=>{
+					job = new ScrapeJob('07/04/1776', 0);
+					const run = await job.run();
+
+					const bag: Bag<Call> = job.getCalls();
+
+					assert.strictEqual(bag.size(), 0);
+				});
+
+			});	
+
+			describe('databaseAllCalls', function(){
+				it('adds every call in the bag to the db', async ()=>{
+
+					await job.run();
+
+					const callDbStub = sinon.stub(Call.prototype, 'addToDb');
+
+					await job.DatabaseAllCalls();
+
+					callDbStub.restore();
+
+					sinon.assert.called(callDbStub);
+				});
+			});		
+		}
 
 		describe('getCalls', ()=>{
 			it('returns a Bag', ()=>{
@@ -53,18 +59,18 @@ describe('ScrapeJob', function(){
 			});
 		});
 
-		describe('GetCallsAsCSVString', ()=>{
-			it.skip('returns a string', async ()=>{
-				const run = await job.run();
-				const csv = await job.GetCallsAsCSVString();
+		// describe('GetCallsAsCSVString', ()=>{
+		// 	it.skip('returns a string', async ()=>{
+		// 		const run = await job.run();
+		// 		const csv = await job.GetCallsAsCSVString();
 
-				assert.typeOf(csv, 'string');
-			});
+		// 		assert.typeOf(csv, 'string');
+		// 	});
 
-			it('is an empty string if the bag of calls is empty', async ()=>{
-				const csv = await job.GetCallsAsCSVString();
-				assert.strictEqual(csv, '');
-			});
-		});
+		// 	it('is an empty string if the bag of calls is empty', async ()=>{
+		// 		const csv = await job.GetCallsAsCSVString();
+		// 		assert.strictEqual(csv, '');
+		// 	});
+		// });
 	});
 });
