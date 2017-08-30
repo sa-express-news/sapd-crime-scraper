f29fbe5a0367fa13e0e1c2cb8d136fb3fbb51e77
Kia Farhang <kfarhang0@gmail.com>
2017-08-30 13:52:41 -0500

    Added more realistic timeouts for integration tests

--- src/Integration Tests/spec.ts
+++ src/Integration Tests/spec.ts
@@ -8,12 +8,15 @@ const assert = chai.assert;
 
 if (process.env.TEST_NETWORK === 'true'){
 	describe('Sample Scrapes', function(){
+		before(async()=>{
+			await CallModel.destroy({where: {}});
+		})
 		afterEach(async()=>{
 			await CallModel.destroy({where: {}});
 		});
 		describe('July 1-2, 2017, district 3', ()=>{
 			it('should add 581 calls to the database', async function(){
-				this.timeout(10000000000);
+				this.timeout(60000);
 				try{
 					const machine = new SAPDScrapeMachine({
 						startDate: new Date('7/1/2017'),
@@ -42,7 +45,7 @@ if (process.env.TEST_NETWORK === 'true'){
 
 		describe('March 1-7, 2011, districts 0-2', ()=>{
 			it('should add 4,305 calls to the database', async function(){
-				this.timeout(10000000000);
+				this.timeout(240000);
 				try{
 					const machine = new SAPDScrapeMachine({
 						startDate: new Date('3/1/2011'),
@@ -69,7 +72,7 @@ if (process.env.TEST_NETWORK === 'true'){
 
 		describe('May 2013, district 10', ()=>{
 			it('should add 5,173 calls to the database', async function(){
-				this.timeout(10000000000);
+				this.timeout(240000);
 				try{
 					const machine = new SAPDScrapeMachine({
 						startDate: new Date('5/1/2013'),
