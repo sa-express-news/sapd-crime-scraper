217fe8cae4c227d51d2c9ff7e77c919287c4089d
Kia Farhang <kfarhang0@gmail.com>
2017-10-09 09:45:12 -0500

    Refactored GetRequest test suite to use nock rather than making a real network request

--- src/Classes/Concrete/GetRequest/spec.ts
+++ src/Classes/Concrete/GetRequest/spec.ts
@@ -1,26 +1,33 @@
 import { GetRequest } from './index';
 import * as chai from 'chai';
+import * as nock from 'nock';
+import * as path from 'path';
 import 'mocha';
 const assert = chai.assert;
 
-describe('GetRequest', ()=>{
-	describe('Methods', ()=>{
-		if(process.env.NETWORK_TEST === 'true'){
-			describe('get', ()=>{
-				it('should return a Document', async function(){
-					this.timeout(5000);
+describe('GetRequest', () => {
+	describe('Methods', () => {
+		after(() => {
+			nock.cleanAll();
+		})
+		const fakeServer = nock('https://example.com')
+			.persist()
+			.get('/')
+			.replyWithFile(200, path.join(__dirname, '/../../../../src/Classes/Concrete/GetRequest/example.html'));
+		describe('get', () => {
+			it('should return a Document', async function () {
+				this.timeout(5000);
 
-					const request = new GetRequest();
+				const request = new GetRequest();
 
-					const document = await request.get('https://example.com');
+				const document = await request.get('https://example.com');
 
-					assert.typeOf(document, 'Document');
-				});
+				assert.typeOf(document, 'Document');
 			});
-		}
+		});
 
-		describe('getCookieJar', ()=>{
-			it('should return an object', ()=>{
+		describe('getCookieJar', () => {
+			it('should return an object', () => {
 				const request = new GetRequest();
 
 				assert.isObject(request.getCookieJar());
