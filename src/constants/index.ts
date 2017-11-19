422f11cd41a545a2aa951bf61a5547b086ca42e8
Kia Farhang <kfarhang0@gmail.com>
2017-11-19 13:33:32 -0600

    Add constants file, which contains the parts of the SAPD form that never change

---
+++ src/constants/index.ts
@@ -0,0 +1,16 @@
+export const SAPDFormParamConstants = {
+    ToolkitScriptManager1_HiddenField: ';;AjaxControlToolkit, Version=3.5.60623.0, Culture=neutral, PublicKeyToken=28f01b0e84b6d53e:en-US:834c499a-b613-438c-a778-d32ab4976134:f9cec9bc:de1feab2:f2c8e708:720a52bf:589eaa30:698129cf:d9d4bb33:fcf0e993:fb9b4c57:ccb96cf9',
+    __EVENTTARGET: '',
+    __EVENTARGUMENT: '',
+    __LASTFOCUS: '',
+    __VIEWSTATE: '',
+    __VIEWSTATEGENERATOR: '',
+    __EVENTVALIDATION: '',
+    rdbSearchRange: 'day',
+    txtZipcode: '',
+    ddlCategory: 'ALL ',
+    ddlSchoolDistrict: ' ',
+    cbxHOA$cbxHOA_TextBox: '',
+    cbxHOA$cbxHOA_HiddenField: '-1',
+    btnSearch: 'View Data'
+}
