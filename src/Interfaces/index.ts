3a5838dc8dbb550134d80353d19588fee7577913
Kia Farhang <kfarhang0@gmail.com>
2017-11-22 13:37:03 -0600

    Add interface for the SAPD POST form params and the three variables of it that are controlled by scraper user input

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -30,3 +30,29 @@ export interface Call {
     councilDistrict: number;
     zipcode: number;
 }
+
+export interface SAPDFormParams {
+    ToolkitScriptManager1_HiddenField: string;
+    __EVENTTARGET: string;
+    __EVENTARGUMENT: string;
+    __LASTFOCUS: string;
+    __VIEWSTATE: string;
+    __VIEWSTATEGENERATOR: string;
+    __EVENTVALIDATION: string;
+    txtStart: string;
+    rdbSearchRange: string;
+    txtEndDate: string;
+    txtZipcode: string;
+    ddlCategory: string;
+    ddlCouncilDistrict: string;
+    ddlSchoolDistrict: string;
+    cbxHOA$cbxHOA_TextBox: string;
+    cbxHOA$cbxHOA_HiddenField: string;
+    btnSearch: string;
+}
+
+export interface VariableSAPDFormParams {
+    txtStart: string;
+    txtEndDate: string;
+    ddlCouncilDistrict: string;
+}
