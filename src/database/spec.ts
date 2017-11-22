30ad321608710587bdad25662f9badc16c01f3e8
Kia Farhang <kfarhang0@gmail.com>
2017-11-22 14:44:12 -0600

    Add database test suite to 'Database' wrapping block

--- src/database/spec.ts
+++ src/database/spec.ts
@@ -7,50 +7,52 @@ dotenv.config();
 
 import { pgp, db, upsertReplaceQuery } from './index'
 
-describe('upsertReplaceQuery', () => {
-    after(async () => {
-        await db.query(`DELETE FROM empty;`);
-    });
-    const colSet = new pgp.helpers.ColumnSet([
-        { name: 'id' },
-        { name: 'foo' }
-    ], { table: { table: 'empty' } });
+describe('Database', () => {
+    describe('upsertReplaceQuery', () => {
+        after(async () => {
+            await db.query(`DELETE FROM empty;`);
+        });
+        const colSet = new pgp.helpers.ColumnSet([
+            { name: 'id' },
+            { name: 'foo' }
+        ], { table: { table: 'empty' } });
 
-    const data = [{ id: 1, foo: 'bar' }, { id: 2, foo: 'baz' }];
-    it('returns a string', () => {
+        const data = [{ id: 1, foo: 'bar' }, { id: 2, foo: 'baz' }];
+        it('returns a string', () => {
 
-        const queryString = upsertReplaceQuery(data, colSet, 'id');
+            const queryString = upsertReplaceQuery(data, colSet, 'id');
 
-        assert.isString(queryString);
-    });
-    it('the string is a valid SQL query that inserts the data if not there already', async () => {
-        const queryString = upsertReplaceQuery(data, colSet, 'id');
+            assert.isString(queryString);
+        });
+        it('the string is a valid SQL query that inserts the data if not there already', async () => {
+            const queryString = upsertReplaceQuery(data, colSet, 'id');
 
-        await db.query(queryString);
+            await db.query(queryString);
 
-        const result = await db.query('SELECT * FROM empty');
+            const result = await db.query('SELECT * FROM empty');
 
-        assert.lengthOf(result, 2);
-        const row1 = result[0], row2 = result[1];
-        assert.strictEqual(row1.id, 1);
-        assert.strictEqual(row1.foo, 'bar');
-        assert.strictEqual(row2.id, 2);
-        assert.strictEqual(row2.foo, 'baz');
-    });
-    it(`the query updates the data if there's a conflict on the constraint provided`, async () => {
-        const conflictingData = [{ id: 1, foo: 'buzz' }, { id: 2, foo: 'bat' }];
+            assert.lengthOf(result, 2);
+            const row1 = result[0], row2 = result[1];
+            assert.strictEqual(row1.id, 1);
+            assert.strictEqual(row1.foo, 'bar');
+            assert.strictEqual(row2.id, 2);
+            assert.strictEqual(row2.foo, 'baz');
+        });
+        it(`the query updates the data if there's a conflict on the constraint provided`, async () => {
+            const conflictingData = [{ id: 1, foo: 'buzz' }, { id: 2, foo: 'bat' }];
 
-        const firstQuery = upsertReplaceQuery(data, colSet, 'id');
-        const secondQuery = upsertReplaceQuery(conflictingData, colSet, 'id');
+            const firstQuery = upsertReplaceQuery(data, colSet, 'id');
+            const secondQuery = upsertReplaceQuery(conflictingData, colSet, 'id');
 
-        await db.query(firstQuery);
-        await db.query(secondQuery);
+            await db.query(firstQuery);
+            await db.query(secondQuery);
 
-        const result = await db.query('SELECT * FROM empty');
-        const row1 = result[0], row2 = result[1];
-        assert.strictEqual(row1.id, 1);
-        assert.strictEqual(row1.foo, 'buzz');
-        assert.strictEqual(row2.id, 2);
-        assert.strictEqual(row2.foo, 'bat');
+            const result = await db.query('SELECT * FROM empty');
+            const row1 = result[0], row2 = result[1];
+            assert.strictEqual(row1.id, 1);
+            assert.strictEqual(row1.foo, 'buzz');
+            assert.strictEqual(row2.id, 2);
+            assert.strictEqual(row2.foo, 'bat');
+        });
     });
 });
