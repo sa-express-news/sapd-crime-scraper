ee3a48e3d45bddc79bb9643033be014f36288bbc
Kia Farhang <kfarhang0@gmail.com>
2017-11-23 12:42:51 -0600

    Uncomment some scraper tests that were turned off to test something

--- src/scraper/spec.ts
+++ src/scraper/spec.ts
@@ -86,98 +86,98 @@ describe('Scraper', () => {
             });
         });
     });
-    // describe('runScrapeJob', () => {
-    //     const reportsEndpoint = `https://webapp3.sanantonio.gov/`;
-    //     const resultsEndpoint = `https://webapp3.sanantonio.gov/`;
-    //     before(() => {
-    //         const reportsGetServer = nock(reportsEndpoint)
-    //             .defaultReplyHeaders({
-    //                 'Cookie': 'ASP.NET_SessionId=3lejdamtppr0iseefz5czqy2'
-    //             })
-    //             .persist()
-    //             .get('/policecalls/Reports.aspx')
-    //             .replyWithFile(200, path.join(__dirname, '/../../src/scraper/reports.html'));
+    describe('runScrapeJob', () => {
+        const reportsEndpoint = `https://webapp3.sanantonio.gov/`;
+        const resultsEndpoint = `https://webapp3.sanantonio.gov/`;
+        before(() => {
+            const reportsGetServer = nock(reportsEndpoint)
+                .defaultReplyHeaders({
+                    'Cookie': 'ASP.NET_SessionId=3lejdamtppr0iseefz5czqy2'
+                })
+                .persist()
+                .get('/policecalls/Reports.aspx')
+                .replyWithFile(200, path.join(__dirname, '/../../src/scraper/reports.html'));
 
-    //         const reportsPostServer = nock(reportsEndpoint)
-    //             .persist()
-    //             .post('/policecalls/Reports.aspx', {
-    //                 ToolkitScriptManager1_HiddenField: ';;AjaxControlToolkit, Version=3.5.60623.0, Culture=neutral, PublicKeyToken=28f01b0e84b6d53e:en-US:834c499a-b613-438c-a778-d32ab4976134:f9cec9bc:de1feab2:f2c8e708:720a52bf:589eaa30:698129cf:d9d4bb33:fcf0e993:fb9b4c57:ccb96cf9',
-    //                 __EVENTTARGET: '',
-    //                 __EVENTARGUMENT: '',
-    //                 __LASTFOCUS: '',
-    //                 __VIEWSTATE: viewState,
-    //                 __VIEWSTATEGENERATOR: viewStateGenerator,
-    //                 __EVENTVALIDATION: eventValidation,
-    //                 rdbSearchRange: 'day',
-    //                 txtZipcode: '',
-    //                 ddlCategory: 'ALL ',
-    //                 ddlSchoolDistrict: ' ',
-    //                 cbxHOA$cbxHOA_TextBox: '',
-    //                 cbxHOA$cbxHOA_HiddenField: '-1',
-    //                 btnSearch: 'View Data',
-    //                 txtStart: '10/01/2017',
-    //                 txtEndDate: '10/01/2017',
-    //                 ddlCouncilDistrict: '1'
-    //             })
-    //             .reply(200);
+            const reportsPostServer = nock(reportsEndpoint)
+                .persist()
+                .post('/policecalls/Reports.aspx', {
+                    ToolkitScriptManager1_HiddenField: ';;AjaxControlToolkit, Version=3.5.60623.0, Culture=neutral, PublicKeyToken=28f01b0e84b6d53e:en-US:834c499a-b613-438c-a778-d32ab4976134:f9cec9bc:de1feab2:f2c8e708:720a52bf:589eaa30:698129cf:d9d4bb33:fcf0e993:fb9b4c57:ccb96cf9',
+                    __EVENTTARGET: '',
+                    __EVENTARGUMENT: '',
+                    __LASTFOCUS: '',
+                    __VIEWSTATE: viewState,
+                    __VIEWSTATEGENERATOR: viewStateGenerator,
+                    __EVENTVALIDATION: eventValidation,
+                    rdbSearchRange: 'day',
+                    txtZipcode: '',
+                    ddlCategory: 'ALL ',
+                    ddlSchoolDistrict: ' ',
+                    cbxHOA$cbxHOA_TextBox: '',
+                    cbxHOA$cbxHOA_HiddenField: '-1',
+                    btnSearch: 'View Data',
+                    txtStart: '10/01/2017',
+                    txtEndDate: '10/01/2017',
+                    ddlCouncilDistrict: '1'
+                })
+                .reply(200);
 
-    //         const resultsGetServer = nock(resultsEndpoint, {
-    //             reqheaders: {
-    //             }
-    //         })
-    //             .persist()
-    //             .get('/policecalls/Results.aspx')
-    //             .replyWithFile(200, path.join(__dirname, '/../../src/scraper/results.html'));
-    //     });
-    //     after(() => {
-    //         nock.cleanAll();
-    //     });
+            const resultsGetServer = nock(resultsEndpoint, {
+                reqheaders: {
+                }
+            })
+                .persist()
+                .get('/policecalls/Results.aspx')
+                .replyWithFile(200, path.join(__dirname, '/../../src/scraper/results.html'));
+        });
+        after(() => {
+            nock.cleanAll();
+        });
 
-    //     it('throws an error if passed a date before 1/1/11', async () => {
-    //         const earlyDate = new Date('12/31/2010');
-    //         let err;
+        it('throws an error if passed a date before 1/1/11', async () => {
+            const earlyDate = new Date('12/31/2010');
+            let err;
 
-    //         try {
-    //             const calls = await scraper.runScrapeJob(earlyDate, 0);
-    //         } catch (e) {
-    //             err = e;
-    //         }
+            try {
+                const calls = await scraper.runScrapeJob(earlyDate, 0);
+            } catch (e) {
+                err = e;
+            }
 
-    //         assert.typeOf(err, 'Error');
-    //     });
-    //     describe('Successful request', () => {
-    //         it('returns an array of Calls', async () => {
-    //             const calls = await scraper.runScrapeJob(new Date('10/01/2017'), 1);
-    //             assert.isArray(calls);
-    //             calls.forEach((call) => {
-    //                 assert.isTrue(scraper.isCall(call));
-    //             });
-    //         });
-    //         it('scrapes every call the web application provides', async () => {
-    //             const calls = await scraper.runScrapeJob(new Date('10/01/2017'), 1);
-    //             // Running a search for district 1 on 10/01/2017 on the actual web app will give you 443 results
-    //             assert.lengthOf(calls, 443);
-    //         });
-    //     });
-    //     describe('Unsuccessful requests', () => {
-    //         it('throws an error', async () => {
-    //             const postFormStub = sinon.stub(network, 'postForm');
-    //             postFormStub.throws();
+            assert.typeOf(err, 'Error');
+        });
+        describe('Successful request', () => {
+            it('returns an array of Calls', async () => {
+                const calls = await scraper.runScrapeJob(new Date('10/01/2017'), 1);
+                assert.isArray(calls);
+                calls.forEach((call) => {
+                    assert.isTrue(scraper.isCall(call));
+                });
+            });
+            it('scrapes every call the web application provides', async () => {
+                const calls = await scraper.runScrapeJob(new Date('10/01/2017'), 1);
+                // Running a search for district 1 on 10/01/2017 on the actual web app will give you 443 results
+                assert.lengthOf(calls, 443);
+            });
+        });
+        describe('Unsuccessful requests', () => {
+            it('throws an error', async () => {
+                const postFormStub = sinon.stub(network, 'postForm');
+                postFormStub.throws();
 
-    //             let err;
+                let err;
 
-    //             try {
-    //                 const calls = await scraper.runScrapeJob(new Date('10/01/2017'), 1);
-    //             } catch (e) {
-    //                 err = e;
-    //             }
+                try {
+                    const calls = await scraper.runScrapeJob(new Date('10/01/2017'), 1);
+                } catch (e) {
+                    err = e;
+                }
 
-    //             postFormStub.restore();
+                postFormStub.restore();
 
-    //             assert.typeOf(err, 'Error');
-    //         });
-    //     });
-    // });
+                assert.typeOf(err, 'Error');
+            });
+        });
+    });
     describe('runScrapeQueue', () => {
         it('throws an error if either the start or end date passed is before 1/1/2011', async () => {
             const startDate = new Date('10/1/2010');
