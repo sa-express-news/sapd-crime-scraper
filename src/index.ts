6777740104c4190f6db5666100df1fe9e943ae3e
Kia Farhang <kfarhang0@gmail.com>
2017-11-23 12:43:29 -0600

    Update main process to scrape the previous month unless passed a command-line flag to scrape everything; add an npm script full-scrape to trigger the full scrape process

--- src/index.ts
+++ src/index.ts
@@ -1,100 +1,38 @@
 import * as dotenv from 'dotenv';
-
-import { ScrapeJob } from './Classes/Concrete/ScrapeJob';
-import { SAPDScrapeMachine } from './Classes/Concrete/SAPDScrapeMachine';
-import { PreviousMonthScrape } from './Classes/Concrete/PreviousMonthScrape';
+import { runScrapeQueue } from './scraper';
+import { db } from './database';
+import { getFirstOfPreviousMonth, getLastOfPreviousMonth } from './util';
 
 dotenv.config();
 
-const db: string = process.env.NODE_ENV === 'test' ? process.env.TEST_DB : process.env.DB;
+export const main = async (): Promise<void> => {
+    const districts = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-async function syncTable() {
-	// await CallModel.sync();
-	// // sequelize.close();
+    if (process.env.FULL_SCRAPE === 'TRUE') {
+        const startDate = new Date('1/1/2011');
+        const endDate = getLastOfPreviousMonth(getLastOfPreviousMonth(new Date()));
+
+        try {
+            await runScrapeQueue(startDate, endDate, districts, db);
+            console.log(`Scraped every call from 1/1/2011 to ${endDate.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' })}`);
+        } catch (e) {
+            throw new Error(e);
+        }
+
+    } else {
+        // Scrapes every call recorded for the previous month
+
+        const today = new Date();
+        const firstOfLastMonth = getFirstOfPreviousMonth(today);
+        const lastOfLastMonth = getLastOfPreviousMonth(today);
+
+        try {
+            await runScrapeQueue(firstOfLastMonth, lastOfLastMonth, districts, db);
+            console.log(`Scraped every call from ${firstOfLastMonth.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' })} to ${lastOfLastMonth.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' })}`);
+        } catch (e) {
+            throw new Error(e);
+        }
+    }
 }
 
-// syncTable();
-
-
-// export const main : Function = async()=>{
-// 	switch(process.argv[3]){
-// 		case 'monthly':
-// 		const machine: SAPDScrapeMachine = new PreviousMonthScrape(new Date()).getMachine();
-// 		console.log('Queueing scrape jobs');
-// 		await machine.QueueJobs();
-// 		console.log('All jobs queued!');
-// 		try{
-// 			console.log('Running scrape jobs. This may take a while...');
-// 			await machine.runJobs();
-// 			console.log('Successfully scraped the SAPD website!');
-// 			const jobs = machine.getJobs();
-// 			console.log('Time to add calls to the database. This will also take a while...')
-// 			for(let job of jobs){
-// 				await job.DatabaseAllCalls();
-// 			}
-// 			console.log('Successfully added calls to the database!');
-// 			sequelize.close();
-// 		}catch(e){
-// 			console.log(`There was an error running the scrape job: ${e}`);
-// 			throw new Error(e);
-// 		}
-// 	}
-// }
-
-// main();
-
-
-
-// const scrape = new ScrapeJob('06/08/2017');
-
-// async function a(){
-// 	const b = await scrape.run();
-// 	const c = await scrape.GetCallsAsCSVString();
-// 	// sequelize.close();
-// }
-
-// a();
-
-
-
-// const machine = new SAPDScrapeMachine({
-// 	startDate: new Date('05/01/2017'),
-// 	endDate: new Date('05/01/2017'),
-// 	districts: [1,2]
-// });
-
-// console.log(machine);
-
-// async function b(){
-// 	await machine.QueueJobs();
-// 	await machine.runJobs();
-
-// 	const jobs = machine.getJobs();
-
-// 	for (let job of jobs){
-// 		await job.DatabaseAllCalls();
-// 	}
-
-// 	sequelize.close();
-// }
-
-// b();
-
-
-// for (let job of jobs){
-// 	console.log(job.config);
-// }
-
-
-
-// import * as fs from 'fs';
-
-// const c = async()=>{
-// 	await fs.mkdir('results', '777', (err)=>{
-// 		if(err){
-// 			console.log('folder exists');
-// 		}
-// 	});
-// }
-
-// c();
+main();
